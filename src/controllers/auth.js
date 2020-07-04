9e86e80dd0d16115cc5a000b306e872de3ab6531
Elishafan Machado <elishafan.machado@gmail.com>
2020-07-04 17:48:26 -0300

    storing account into database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,27 @@
 const express = require("express")
 
+const { Account } = require('../models')
+
+const bcrypt = require('bcrypt')
+
 const router = express.Router()
+
+const saltRounds = 10
 
 router.get('/sign-in', (req, resp) => {
     return resp.json('Sign-in')
 })
 
-router.get('/sign-up', (req, resp) => {
-    return resp.json('Sign-up')
+router.get('/sign-up', async (req, resp) => {
+    const email = "elishafan.machado@gmail.com"
+    const password = "123456"
+
+   
+    const hash = bcrypt.hashSync(password, saltRounds)
+
+    const result = await Account.create({ email, password: hash })
+    console.log(hash)
+    return resp.json(result)
 })
 
 module.exports = router
